dd1edd6c3139a3f762ee59b6988d8ba9f4116996
czfdcn <steven.hartley@gm.com>
2024-02-02 16:04:24 -0500

    Different proposal that is simpler

--- src/main/java/org/eclipse/uprotocol/uri/factory/UUriFactory.java
+++ src/main/java/org/eclipse/uprotocol/uri/factory/UUriFactory.java
@@ -1,43 +1,40 @@
 package org.eclipse.uprotocol.uri.factory;
 
-import java.util.List;
-
+import org.eclipse.uprotocol.TopicMetadata;
 import org.eclipse.uprotocol.UprotocolOptions;
-import org.eclipse.uprotocol.v1.Topic;
 import org.eclipse.uprotocol.v1.UEntity;
 import org.eclipse.uprotocol.v1.UResource;
 import org.eclipse.uprotocol.v1.UUri;
 
+import com.google.protobuf.DescriptorProtos.EnumValueOptions;
 import com.google.protobuf.DescriptorProtos.ServiceOptions;
+import com.google.protobuf.Descriptors.EnumValueDescriptor;
 import com.google.protobuf.Descriptors.ServiceDescriptor;
 
 public interface UUriFactory {
-    static UUri fromProto(ServiceDescriptor descriptor, Integer topicId) {
-        if (descriptor == null) {
+    static UUri fromProto(ServiceDescriptor service, EnumValueDescriptor value) {
+        if (service == null || value == null) {
             return UUri.getDefaultInstance();
         }
 
-        ServiceOptions options = descriptor.getOptions();
+        ServiceOptions options = service.getOptions();
+        EnumValueOptions valueOptions = value.getOptions();
 
         UEntity entity = UEntity.newBuilder()
             .setName(options.<String>getExtension(UprotocolOptions.name))
             .setId(options.<Integer>getExtension(UprotocolOptions.id))
             .setVersionMajor(options.<Integer>getExtension(UprotocolOptions.versionMajor))
             .build();
-        Topic topic = options.getExtension(UprotocolOptions.topic).stream()
-            .filter(t -> t.getId() == topicId)
-            .findFirst()
-            .orElse(null);
+        TopicMetadata metadata = valueOptions.getExtension(UprotocolOptions.topicMetadata);
         
-            if (topic == null) {
+            if (metadata == null) {
             return UUri.getDefaultInstance();
         }
 
         UResource resource = UResource.newBuilder()
-            .setId(topic.getId()) 
-            .setName(topic.getResources(0))           
-            .setMessage(topic.getMessage())
-            .setInstance(topic.getResources(0))
+            .setId(metadata.getId()) 
+            .setName(value.getName())           
+            .setMessage(metadata.getMessage())
             .build();    
         return UUri.newBuilder()
             .setEntity(entity)
--- src/test/java/org/eclipse/uprotocol/uri/factory/UUriFactoryTest.java
+++ src/test/java/org/eclipse/uprotocol/uri/factory/UUriFactoryTest.java
@@ -2,21 +2,20 @@ package org.eclipse.uprotocol.uri.factory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.eclipse.uprotocol.core.usubscription.v3.Topic;
 import org.eclipse.uprotocol.core.usubscription.v3.USubscriptionProto;
 import org.eclipse.uprotocol.core.usubscription.v3.Update;
-import org.eclipse.uprotocol.core.usubscription.v3.Update.Resources;
 import org.eclipse.uprotocol.v1.UUri;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import com.google.protobuf.Descriptors.ServiceDescriptor;
 
 public class UUriFactoryTest {
     @Test
     @DisplayName("Test build valid usubscription notification topic")
     public void test_build_valid_usubscription_uentity() {
-        ServiceDescriptor descriptor = USubscriptionProto.getDescriptor().getServices().get(0);
-        UUri uri = UUriFactory.fromProto(descriptor, 1000);
+        UUri uri = UUriFactory.fromProto(
+            USubscriptionProto.getDescriptor().getServices().get(0), 
+            Topic.TOPIC_SUBSCRIPTION_CHANGE.getValueDescriptor());
 
         assertEquals(uri.getEntity().getName(), "core.usubscription");
         assertEquals(uri.getEntity().getId(), 0);
@@ -24,6 +23,6 @@ public class UUriFactoryTest {
         assertEquals(uri.getEntity().getVersionMinor(), 0);
         assertEquals(uri.getResource().getId(), 1000);
         assertEquals(uri.getResource().getMessage(), Update.getDescriptor().getName());
-        assertEquals(uri.getResource().getName(), Resources.getDescriptor().findValueByNumber(Resources.subscriptions_VALUE).getName());
+        assertEquals(uri.getResource().getName(), Topic.TOPIC_SUBSCRIPTION_CHANGE.getValueDescriptor().getName());
     }
 }
