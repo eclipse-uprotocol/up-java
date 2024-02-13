101380b93e657945096e09526d3df9b369c28bfc
Jeff Tindall, CSSLP <jefftindall@users.noreply.github.com>
2024-02-13 16:06:57 -0500

    Add traceparent to UCloudEventAttributes (#69)
    * Add traceparent to UCloudEventAttributes
    
    * Get unit project to build and unit tests to pass.
    
    * Make traceparent optional in the constructor
    Per PR comment.
    
    * Remove traceparent from the constructor
    
    * Update factory class to support traceparent
    
    * Added support for traceparent in UAttributedBuilder
    Updated build configuration to allow local testing from a forked repo to support validating unofficial releases.
    
    * Update pom.xml
    
    The git rules got messed up so fixing it.
    
    ---------
    
    Co-authored-by: Steven Hartley <steven.hartley@gm.com>

--- src/test/java/org/eclipse/uprotocol/cloudevent/datamodel/UCloudEventAttributesTest.java
+++ src/test/java/org/eclipse/uprotocol/cloudevent/datamodel/UCloudEventAttributesTest.java
@@ -40,7 +40,7 @@ class UCloudEventAttributesTest {
     }
 
     @Test
-    @DisplayName("Make sure the toString works")
+    @DisplayName("Make sure the default toString works")
     public void testToString() {
         final UCloudEventAttributes uCloudEventAttributes = new UCloudEventAttributes.UCloudEventAttributesBuilder()
                 .withHash("somehash")
@@ -50,7 +50,20 @@ class UCloudEventAttributesTest {
                 .build();
         String expected = "UCloudEventAttributes{hash='somehash', priority=UPRIORITY_CS1, ttl=3, token='someOAuthToken'}";
         assertEquals(expected, uCloudEventAttributes.toString());
+    }
 
+    @Test
+    @DisplayName("Make sure the toString works when all properties are filled")
+    public void testToStringComplete() {
+        final UCloudEventAttributes uCloudEventAttributes = new UCloudEventAttributes.UCloudEventAttributesBuilder()
+                .withHash("somehash")
+                .withPriority(UPriority.UPRIORITY_CS1)
+                .withTtl(3)
+                .withToken("someOAuthToken")
+                .withTraceparent("darthvader")
+                .build();
+        String expected = "UCloudEventAttributes{hash='somehash', priority=UPRIORITY_CS1, ttl=3, token='someOAuthToken', traceparent='darthvader'}";
+        assertEquals(expected, uCloudEventAttributes.toString());
     }
 
     @Test
